e2337689b7d3b1849545adbd4a8a830942e912b6
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2022-01-05 09:47:44 +0100

    datastuff ting

--- dataStuff.py
+++ dataStuff.py
@@ -7,23 +7,25 @@ max_input_neurons = 7 #72
 max_output_neurons = 2
 max_hidden_layers = 2
 max_hidden_neurons = 16 #60
-max_training_sets = 400
+max_training_sets = 300
 learning_rate = 0.01
 max_rounds = 5000 #00
 
 
 # Initialize the tensors from data file
-data = np.loadtxt('data-VOO.csv', delimiter=',')
+data = np.loadtxt('data-VOO.csv', delimiter=',', skiprows=1, usecols= (1,2,3,4,5) )
 fulldata = data.copy()
 
 trainingsets = np.array([])
+print(data)
 
-for i in max_training_sets:
-    x = data[4, 0:max_input_neurons]
-    y = [1 if data[4, max_input_neurons + 1] > x[-1] else 0, 1 if data[4, max_input_neurons + 1] < x[-1] else 0]
-    trainingsets = np.append(trainingsets, np.array([x, y]))
 
-    for i in max_input_neurons:
+for i in range(max_training_sets):
+    x = data[0, 0:max_input_neurons]
+    y = np.array([1 if data[max_input_neurons + 1][0] > x[-1] else 0, 1 if data[max_input_neurons + 1][0] < x[-1] else 0])
+    trainingsets = np.append(trainingsets, np.array([x, y]), axis=0)
+
+    for i in range(max_input_neurons):
         data = np.delete(data, (0), axis=0)
 
 
