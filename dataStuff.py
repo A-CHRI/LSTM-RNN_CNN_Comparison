cad194ff5ad20342b7a1f4b96ab719a14a82ab49
Alexander Anker Christensen <alexander.Anker.chr@gmail.com>
2022-01-05 10:58:45 +0100

    Checked segmentaion

--- dataStuff.py
+++ dataStuff.py
@@ -7,7 +7,7 @@ max_input_neurons = 7 #72
 max_output_neurons = 2
 max_hidden_layers = 2
 max_hidden_neurons = 16 #60
-max_training_sets = 300
+max_training_sets = 400
 learning_rate = 0.01
 max_rounds = 5000 #00
 
@@ -19,11 +19,9 @@ fulldata = data.copy()
 trainingsets = []
 print(data)
 
-
-for i in range(max_training_sets):
-    x = data[max_input_neurons*i:max_input_neurons*(i+1), 0]
-    y = np.array([1 if data[max_input_neurons*(i+1) + 1][0] > x[-1] else 0, 1 if data[max_input_neurons*(i+1) + 1][0] < x[-1] else 0])
+while len(trainingsets) < max_training_sets and (len(trainingsets) + 1)*(max_input_neurons) < len(data):
+    x = data[max_input_neurons*len(trainingsets):max_input_neurons*(len(trainingsets)+1), 0]
+    y = np.array([1 if data[max_input_neurons*(len(trainingsets)+1) + 1][0] > x[-1] else 0, 1 if data[max_input_neurons*(len(trainingsets)+1) + 1][0] < x[-1] else 0])
     trainingsets.append([x, y])
-
 
 print(trainingsets)
