e267ad83b567fbc70c17014da299849cc6899853
Alexander Anker Christensen <alexander.Anker.chr@gmail.com>
2022-01-05 09:08:36 +0100

    Moved some lines

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ max_rounds = 5000 #00
 data = np.loadtxt('data-VOO.csv', delimiter=',')
 
 x = data[4, 0:max_input_neurons]
-y = [1 if data[4, max_input_neurons + 1] > inp[-1] else 0, 1 if data[4, max_input_neurons + 1] < inp[-1] else 0]
-
 inp = torch.from_numpy(x)
+
+y = [1 if data[4, max_input_neurons + 1] > inp[-1] else 0, 1 if data[4, max_input_neurons + 1] < inp[-1] else 0]
 outp = torch.tensor(y)
 
 
