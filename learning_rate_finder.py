605ced3ce370e3f374fa45f81096211621669dda
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2022-01-11 14:15:10 +0100

    hahaha

--- learning_rate_finder.py
+++ learning_rate_finder.py
@@ -12,7 +12,7 @@ features = 5 # Close, Volume, Open, High, Low (Input_size = 5)
 seq_len = 7 # look back period
 batch_size = 64 # Must be a power of 2
 l_rate = 0.00005
-n_epoch = 512 # Must be divisible by 8
+n_epoch = 64 # Must be divisible by 8
 n_hidden = 24 # 2/3 input neurons
 
 n_input = features * seq_len
@@ -110,72 +110,71 @@ if __name__ == '__main__':
     # Clear the log
     with open("learning_rate_data_CNN.txt", "w") as f:
         f.write("")
-    for i in range(200):
-        learning_rate = 0.01/(2**i)
+    for a in range(25):
+        learning_rate = 0.01/(2**a)
+        for q in range(5):
+            model = CNN(features, n_hidden, n_output, n_layers=2).to(device)
+            loss_fn = nn.MSELoss(reduction='sum')
+            optimizer = torch.optim.Adam(model.parameters(), lr=l_rate)
 
 
-        model = CNN(features, n_hidden, n_output, n_layers=2).to(device)
-        loss_fn = nn.MSELoss(reduction='sum')
-        optimizer = torch.optim.Adam(model.parameters(), lr=l_rate)
+            dataset_train = StockData(training_files)
+            dataloader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True)
 
 
-        dataset_train = StockData(training_files)
-        dataloader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True)
+            Loss = []
+
+            samples = len(dataset_train)
+            iterations = samples // batch_size
+            for epoch in range(n_epoch):
+                for i, (x, y) in enumerate(dataloader_train):
+                    x = x.view(-1, seq_len, features)
+                    y = y.view(-1, 1)
+
+                    # Forward pass
+                    y_pred = model(x)
+                    loss = loss_fn(y_pred, y)
+                    Loss.append(loss.item())
+
+                    # Backward and optimize
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                    if (i+1) % 25 == 0:
+                        loss_sci = "{:.4e}".format(loss.item())
+                        print(f"{f'Epoch {epoch+1}/{n_epoch}':^20} | {f'Step {i+1}/{iterations}':^20} | {f'Loss: {loss_sci}':^20} \r", end="")
 
 
-        Loss = []
+            dataset_test = StockData(test_file)
+            dataloader_test = DataLoader(dataset_test, batch_size=batch_size, shuffle=False)
+            y_pred_plot = np.array([])
 
-        samples = len(dataset_train)
-        iterations = samples // batch_size
-        for epoch in range(n_epoch):
-            for i, (x, y) in enumerate(dataloader_train):
+
+            for i, (x, y) in enumerate(dataloader_test):
                 x = x.view(-1, seq_len, features)
                 y = y.view(-1, 1)
 
                 # Forward pass
                 y_pred = model(x)
+                y_pred_plot = np.append(y_pred_plot, y_pred.cpu().detach().numpy())
                 loss = loss_fn(y_pred, y)
-                Loss.append(loss.item())
-
-                # Backward and optimize
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
 
                 if (i+1) % 25 == 0:
                     loss_sci = "{:.4e}".format(loss.item())
                     print(f"{f'Epoch {epoch+1}/{n_epoch}':^20} | {f'Step {i+1}/{iterations}':^20} | {f'Loss: {loss_sci}':^20} \r", end="")
 
+            losspercent = 0
+            # Prediction data
+            y_pred_plot = dataset_test.scaler.inverse_transform(y_pred_plot.reshape(-1, 1))
+            plot_data = np.loadtxt(test_file[0], delimiter=',', skiprows=1, usecols=(1))[::-1]
+            targets = plot_data[seq_len:]
+            for i, e in enumerate(y_pred_plot):
+                target = targets[i]
+                losspercent = losspercent + abs((e-target)/target)
+            losspercent = (losspercent/len(y_pred_plot))*100
 
-        dataset_test = StockData(test_file)
-        dataloader_test = DataLoader(dataset_test, batch_size=batch_size, shuffle=False)
-        y_pred_plot = np.array([])
-
-
-        for i, (x, y) in enumerate(dataloader_test):
-            x = x.view(-1, seq_len, features)
-            y = y.view(-1, 1)
-
-            # Forward pass
-            y_pred = model(x)
-            y_pred_plot = np.append(y_pred_plot, y_pred.cpu().detach().numpy())
-            loss = loss_fn(y_pred, y)
-
-            if (i+1) % 25 == 0:
-                loss_sci = "{:.4e}".format(loss.item())
-                print(f"{f'Epoch {epoch+1}/{n_epoch}':^20} | {f'Step {i+1}/{iterations}':^20} | {f'Loss: {loss_sci}':^20} \r", end="")
-
-        losspercent = 0
-        # Prediction data
-        y_pred_plot = dataset_test.scaler.inverse_transform(y_pred_plot.reshape(-1, 1))
-        plot_data = np.loadtxt(test_file[0], delimiter=',', skiprows=1, usecols=(1))[::-1]
-        targets = plot_data[seq_len:]
-        for i, e in enumerate(y_pred_plot):
-            target = targets[i]
-            losspercent = losspercent + abs((e-target)/target)
-        losspercent = (losspercent/len(y_pred_plot))*100
-
-        print_and_log(str(round(losspercent, 4)) + " " + str(learning_rate))
+            print_and_log(str(round(losspercent[0], 4)) + " " + str(learning_rate))
 
 
 
