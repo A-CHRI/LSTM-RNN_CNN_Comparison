e920234c551768c520cf88c7c9041b7f3ecf167d
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2022-01-17 10:21:02 +0100

    Experiment

--- learning_rate/mean_finder.py
+++ learning_rate/mean_finder.py
@@ -1,12 +1,9 @@
 import numpy as np
 
-data = np.loadtxt("learning_rate/learning_rate_data_LSTM.txt", delimiter=' ')
-dic = {}
-for i in range(int(len(data)/5)):
-    mean = np.mean(data[i*5:i*5+5, 0])
-    lr = data[i*5, 1]
-    print(f"Learning rate is {lr}, mean error is {mean}%")
-    dic[lr] = mean
-mini = min(dic, key=dic.get)
-print(mini)
-print(dic[mini])
+data = np.loadtxt("out/experiment_data_CNN.txt")
+
+print(np.std(data))
+
+data = np.loadtxt("out/experiment_data_LSTM.txt")
+
+print(np.std(data))
